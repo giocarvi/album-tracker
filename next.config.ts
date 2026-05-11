94a24318c76258f6b60326e05628bf53f9cb3887
Giovanny Carvi <giocarvi@gmail.com>
2026-05-11 15:34:38 -0600

    Agregando rewrites para el subdominio del calendario

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // Si alguien entra usando el dominio del calendario...
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'calendario2026.fullfan.net',
+          },
+        ],
+        // ...lo mandamos "en secreto" a la página del calendario
+        destination: '/calendario',
+      },
+      {
+        // Esto cubre cualquier sub-ruta
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'calendario2026.fullfan.net',
+          },
+        ],
+        destination: '/calendario/:path*',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
